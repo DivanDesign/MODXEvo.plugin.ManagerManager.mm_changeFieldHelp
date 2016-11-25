47eaf1e923b74e71c3f81845cfcb6cf2e56baad7
Ilyas Ronef <ilyas@divandesign.biz>
2016-11-25 18:49:41 +0500

    * Minor changes.

--- mm_changefieldhelp/mm_changefieldhelp.php
+++ mm_changefieldhelp/mm_changefieldhelp.php
@@ -5,29 +5,37 @@
  * 
  * @desc A widget for ManagerManager plugin that allows to change help text that appears near each document field when the icon or comment below template variable is hovered.
  * 
- * @uses ManagerManager plugin 0.5.
+ * @uses MODXEvo.plugin.ManagerManager >= 0.5.
  * 
- * @param $field {string} - The name of the document field (or TV) this should apply to. @required
- * @param $helptext {string} - The new help text. @required
- * @param $roles {comma separated string} - The roles that the widget is applied to (when this parameter is empty then widget is applied to the all roles). Default: ''.
- * @param $templates {comma separated string} - Id of the templates to which this widget is applied (when this parameter is empty then widget is applied to the all templates). Default: ''.
+ * @param $field {string} — The name of the document field (or TV) this should apply to. @required
+ * @param $helptext {string} — The new help text. @required
+ * @param $roles {string_commaSeparated} — The roles that the widget is applied to (when this parameter is empty then widget is applied to the all roles). Default: ''.
+ * @param $templates {string_commaSeparated} — Id of the templates to which this widget is applied (when this parameter is empty then widget is applied to the all templates). Default: ''.
  * 
  * @link http://code.divandesign.biz/modx/mm_changefieldhelp/1.1.2
  * 
- * @copyright 2014
+ * @copyright 2012–2014
  */
 
-function mm_changeFieldHelp($field, $helptext = '', $roles = '', $templates = ''){
+function mm_changeFieldHelp(
+	$field,
+	$helptext = '',
+	$roles = '',
+	$templates = ''
+){
 	global $modx;
 	$e = &$modx->Event;
 	
 	if ($helptext == ''){return;}
 	
 	// if the current page is being edited by someone in the list of roles, and uses a template in the list of templates
-	if ($e->name == 'OnDocFormRender' && useThisRule($roles, $templates)){
+	if (
+		$e->name == 'OnDocFormRender' &&
+		useThisRule($roles, $templates)
+	){
 		global $mm_fields;
 		
-		$output = "//---------- mm_changeFieldHelp :: Begin -----\n";
+		$output = '//---------- mm_changeFieldHelp :: Begin -----'.PHP_EOL;
 		
 		// What type is this field?
 		if (isset($mm_fields[$field])){
@@ -48,14 +56,14 @@ if ($mm_changeFieldHelp_title_comment.length == 0){
 
 $mm_changeFieldHelp_title_comment.html("'.addslashes($helptext).'");
 ';
-				//Or document field
+			//Or document field
 			}else{
 				// Give the help button an ID, and modify the alt/title text
-				$output .= '$j("'.$fieldtype.'[name=\''.$fieldname.'\']").siblings("img[style*=\'cursor:help\']").attr("id", "'.$fieldname.'-help").attr("alt", "'.addslashes($helptext).'").attr("title", "'.addslashes($helptext).'");'."\n";
+				$output .= '$j("'.$fieldtype.'[name=\''.$fieldname.'\']").siblings("img[style*=\'cursor:help\']").attr("id", "'.$fieldname.'-help").attr("alt", "'.addslashes($helptext).'").attr("title", "'.addslashes($helptext).'");'.PHP_EOL;
 			}
 		}
 		
-		$output .= "//---------- mm_changeFieldHelp :: End -----\n";
+		$output .= '//---------- mm_changeFieldHelp :: End -----'.PHP_EOL;
 		
 		$e->output($output);
 	}
