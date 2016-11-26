40d8b34be871bac4464f24663d51e9a9b3c12463
Ilyas Ronef <ilyas@divandesign.biz>
2016-11-26 11:08:05 +0500

    * Refactoring, the widget now uses named parameters (with backward compatibility). * Attention! PHP >= 5.4 is required.

--- mm_changefieldhelp/mm_changefieldhelp.php
+++ mm_changefieldhelp/mm_changefieldhelp.php
@@ -5,42 +5,64 @@
  * 
  * @desc A widget for ManagerManager plugin that allows to change help text that appears near each document field when the icon or comment below template variable is hovered.
  * 
+ * @uses PHP >= 5.4.
  * @uses MODXEvo.plugin.ManagerManager >= 0.7.
  * 
- * @param $fields {string_commaSeparated} — The name(s) of the document field (or TV) this should apply to. @required
- * @param $helpText {string_html} — The new help text. If equals to '', help element will be hidden. @required
- * @param $roles {string_commaSeparated} — The roles that the widget is applied to (when this parameter is empty then widget is applied to the all roles). Default: ''.
- * @param $templates {string_commaSeparated} — Id of the templates to which this widget is applied (when this parameter is empty then widget is applied to the all templates). Default: ''.
+ * @param $params {array_associative|stdClass} — The object of params. @required
+ * @param $params['fields'] {string_commaSeparated} — The name(s) of the document field (or TV) this should apply to. @required
+ * @param $params['helpText'] {string_html} — The new help text. If equals to '', help element will be hidden. @required
+ * @param $params['roles'] {string_commaSeparated} — The roles that the widget is applied to (when this parameter is empty then widget is applied to the all roles). Default: ''.
+ * @param $params['templates'] {string_commaSeparated} — Id of the templates to which this widget is applied (when this parameter is empty then widget is applied to the all templates). Default: ''.
  * 
  * @link http://code.divandesign.biz/modx/mm_changefieldhelp/1.1.2
  * 
  * @copyright 2012–2014
  */
 
-function mm_changeFieldHelp(
-	$fields,
-	$helpText = '',
-	$roles = '',
-	$templates = ''
-){
+function mm_changeFieldHelp($params){
+	//For backward compatibility
+	if (
+		!is_array($params) &&
+		!is_object($params)
+	){
+		//Convert ordered list of params to named
+		$params = ddTools::orderedParamsToNamed([
+			'paramsList' => func_get_args(),
+			'compliance' => [
+				'fields',
+				'helpText',
+				'roles',
+				'templates'
+			]
+		]);
+	}
+	
+	//Defaults
+	$params = (object) array_merge([
+// 		'fields' => '',
+// 		'helpText' => '',
+		'roles' => '',
+		'templates' => ''
+	], (array) $params);
+	
 	global $modx;
 	$e = &$modx->Event;
 	
 	// if the current page is being edited by someone in the list of roles, and uses a template in the list of templates
 	if (
 		$e->name == 'OnDocFormRender' &&
-		useThisRule($roles, $templates)
+		useThisRule($params->roles, $params->templates)
 	){
 		$output = '//---------- mm_changeFieldHelp :: Begin -----'.PHP_EOL;
 		
 		// Clean up for js output
-		$helpText = ddTools::escapeForJS($helpText);
+		$params->helpText = ddTools::escapeForJS($params->helpText);
 		
 		$output .=
 '
-$j.each($j.ddMM.makeArray("'.$fields.'"), function(){
+$j.each($j.ddMM.makeArray("'.$params->fields.'"), function(){
 	var field = $j.ddMM.fields[this],
-		helpText = "'.$helpText.'";
+		helpText = "'.$params->helpText.'";
 	
 	//If the field exists
 	if ($j.isPlainObject(field)){
